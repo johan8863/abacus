4b3cd36b3a8358241b268089867b1f90768ebe01
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 13:59:48 -0400

    feat(apps): installed expenses apps

--- abacus_backend/core/settings/base.py
+++ abacus_backend/core/settings/base.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local
-    'apps.categories'
+    'apps.categories',
+    'apps.expenses',
 ]
 
 MIDDLEWARE = [
