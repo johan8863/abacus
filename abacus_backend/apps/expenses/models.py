e7cf74ddeb244443d6c9aaf76d8f86a0253e4426
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 14:02:00 -0400

    feat(apps): installed expenses models

--- abacus_backend/apps/expenses/models.py
+++ abacus_backend/apps/expenses/models.py
@@ -1,3 +1,25 @@
+"""models in expenses module"""
+
+# django
 from django.db import models
 
-# Create your models here.
+# local
+from ..categories.models import Category
+
+
+class Expense(models.Model):
+    """class to manage expenses in the software"""
+    name = models.CharField(verbose_name="Nombre", max_length=120)
+    cost = models.DecimalField(verbose_name="Importe", max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    is_created_auto = models.BooleanField(default=True)
+    is_updated_auto = models.BooleanField(default=True)
+    created_manual = models.DateField(null=True, blank=True)
+    updated_manual = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Gasto"
+        verbose_name_plural = "Gastos"
+    
+    def __str__(self):
+        return self.name
