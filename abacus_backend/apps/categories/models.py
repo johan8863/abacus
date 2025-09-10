44419b265f303e570979278577fd3efc0505364e
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 13:49:09 -0400

    feat(apps): installed category models

--- abacus_backend/apps/categories/models.py
+++ abacus_backend/apps/categories/models.py
@@ -1,3 +1,17 @@
+"""models in category module"""
+
+# django
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Class to manage expenses categories"""
+    name = models.CharField(verbose_name="Nombre", max_length=50)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+    
+    def __str__(self):
+        return self.name
