14b86a6a044826b140e52e6c4b6347d1adb3bde5
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 14:08:25 -0400

    feat(apps): added pre_save signal to handle manual dates in case of auto flags

--- abacus_backend/apps/expenses/apps.py
+++ abacus_backend/apps/expenses/apps.py
@@ -1,6 +1,12 @@
+"""expenses app config module"""
+
+# django
 from django.apps import AppConfig
 
 
 class ExpensesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.expenses'
+
+    def ready(self):
+        import apps.expenses.signals
---
+++ abacus_backend/apps/expenses/signals.py
@@ -0,0 +1,31 @@
+"""signals from expenses module"""
+
+# python
+from datetime import datetime
+
+# django
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+# local
+from .models import Expense
+
+@receiver(pre_save, sender=Expense)
+def set_expenses_dates(sender, instance, **kwargs):
+    """
+    Signal handler to set manual dates from auto flags
+    """
+    now = datetime.now()
+    
+    # if its a new object
+    if not instance.pk:
+        if instance.is_created_auto:
+            # only assign if not initial value
+            # but always set updated_manual
+            if not instance.created_manual:
+                instance.created_manual = now
+                instance.updated_manual = now
+    
+    # to update
+    if instance.is_updated_auto:
+        instance.updated_manual = now
