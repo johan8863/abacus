5e73795d65336db377c771c7434b228c6c1ea443
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 06:25:57 -0400

    feat(apps): generated category app

--- abacus_backend/core/settings/base.py
+++ abacus_backend/core/settings/base.py
@@ -34,12 +34,15 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default="localhost", cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    # django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'apps.categories'
 ]
 
 MIDDLEWARE = [
