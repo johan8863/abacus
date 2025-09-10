2da1e3338bd4edc6efa015e69e250221321e55b6
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-10 06:16:08 -0400

    feat(settings): initial database creation

--- abacus_backend/core/settings/dev.py
+++ abacus_backend/core/settings/dev.py
@@ -1,5 +1,6 @@
 """dev settings submodule"""
 
+
 # local
 from .base import *
 
@@ -8,7 +9,11 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
