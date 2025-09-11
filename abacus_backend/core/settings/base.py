8e3b80059f1dbfdff646aaf5bb08188485e55a99
Johan Travieso Castro <jtravieso8863@gmail.com>
2025-09-11 05:11:34 -0400

    feat(settings): installed rest framework app

--- abacus_backend/core/settings/base.py
+++ abacus_backend/core/settings/base.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third
+    'rest_framework',
     # local
     'apps.categories',
     'apps.expenses',
